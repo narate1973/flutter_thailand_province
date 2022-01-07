2f3fb1018f6b364db1dbd93c4e3ea72a442e7795
Peam <peammy2010@hotmail.com>
2022-01-07 12:30:46 +0700

    fixed: delete get subdistrict from province

--- lib/features/subdistrict/data/datasources/subdistrict_datasource.dart
+++ lib/features/subdistrict/data/datasources/subdistrict_datasource.dart
@@ -2,7 +2,6 @@ import 'package:thailand_province_phn/core/database/thailand_provinces_data_base
 import 'package:thailand_province_phn/features/subdistrict/data/models/models.dart';
 
 abstract class SubdistrictDataSourceInterface {
-  Future<List<SubdistrictModel>> getSubdistrictsFromProvince(int provinceId);
   Future<List<SubdistrictModel>> getSubdistrictsFromDistrict(int districtId);
   Future<List<SubdistrictModel>> getAllSubdistricts();
 }
@@ -21,19 +20,4 @@ class SubdistrictDataSource implements SubdistrictDataSourceInterface {
               where: 'amphure_id = ?', whereArgs: [districtId]))
           .map((e) => SubdistrictModel.fromJson(e))
           .toList();
-
-  @override
-  Future<List<SubdistrictModel>> getSubdistrictsFromProvince(
-      int provinceId) async {
-    // TODO:datasource
-    final district = await ThailandProvincesDatabase.db
-        .query('amphures', where: 'province_id = ?', whereArgs: [provinceId]);
-    final subdistricts = <SubdistrictModel>[];
-    for (var i in district) {
-      final subdistrict = await ThailandProvincesDatabase.db
-          .query('districts', where: 'amphure_id = ?', whereArgs: [i['id']]);
-      subdistricts.addAll(subdistrict.map((e) => SubdistrictModel.fromJson(e)));
-    }
-    return subdistricts;
-  }
 }
